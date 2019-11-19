b3cf0c0fb145c2afff5354a6a7a79920280cc8ec
Carol Soler <lopezsisters11@gmail.com>
2019-11-19 19:29:24 +0100

    removed city table and city_id in usercities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_133800) do
+ActiveRecord::Schema.define(version: 2019_11_19_174455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,13 +77,20 @@ ActiveRecord::Schema.define(version: 2019_11_19_133800) do
     t.index ["user_id"], name: "index_tips_on_user_id"
   end
 
+  create_table "user_cities", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_cities_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "username"
     t.string "first_name"
     t.string "last_name"
-    t.text "cities", default: [], array: true
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -114,6 +121,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_133800) do
   add_foreign_key "tips", "categories"
   add_foreign_key "tips", "questions"
   add_foreign_key "tips", "users"
+  add_foreign_key "user_cities", "users"
   add_foreign_key "votes", "tips"
   add_foreign_key "votes", "users"
 end
