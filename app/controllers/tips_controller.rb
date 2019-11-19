19fd768b99160d62c8aaf462eac65e551f3b3b89
Ana Maria Benedetti <ambenedetti@gmail.com>
2019-11-19 16:32:33 +0100

    finished new and update for tips

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -3,13 +3,30 @@ class TipsController < ApplicationController
 
   def index
     @tips = policy_scope(Tip)
+  end
 
+  def new
+    @tip = Tip.new
+    authorize @tip
+  end
+
+  def create
+    @tip = Tip.new(tip_params)
+    authorize @tip
+    @tip.user = current_user
+    @tip.status = 0
+    @tip.category = Category.find(params[:tip][:category_id])
+    if @tip.save
+      redirect_to tips_path
+    else
+      render :new
+    end
   end
 
 private
 
   def tip_params
-    params.require(:tip).permit(:content, :city, :title, :latitude, :longitude, :status)
+    params.require(:tip).permit(:content, :city, :title, :latitude, :longitude, :status, :category)
   end
 
   def set_tip
