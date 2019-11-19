4e458623966b68c4af4c10e24bf1fadd33787e5b
Belen Battaglini <belenbattaglini@gmail.com>
2019-11-19 19:46:09 +0100

    Flag started

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,22 @@
 class ReportsController < ApplicationController
+  def new
+    @report = Report.new(report_params)
+    authorize @report
+  end
+
+  def create
+    @report = Report.new(report_params)
+    authorize @report
+    @report.user = current_user
+    if @report.save
+      redirect_to tips_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def report_params
+    params.require(:report).permit(:tip_id, :description, :reason_id)
+  end
 end
