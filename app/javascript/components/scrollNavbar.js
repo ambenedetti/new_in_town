c9237b5e65940fb67b0ce24d1b7812de87eb0e3b
Belen Battaglini <belenbattaglini@gmail.com>
2019-11-25 12:54:21 +0100

    Arrows on scrolling in category navbar

---
+++ app/javascript/components/scrollNavbar.js
@@ -0,0 +1,21 @@
+const navbar = document.querySelector(".nav");
+
+navbar.addEventListener("scroll", (event) => {
+    console.log(event.target.scrollLeft);
+    const arrowRight = document.querySelector(".scroll-right");
+    const arrowLeft = document.querySelector(".scroll-left");
+    const scrollPosition = event.target.scrolllLeft;
+
+      arrowLeft.classList.add("arrow-active");
+      if (event.target.scrollLeft <= 40) {
+        arrowLeft.classList.remove("arrow-active");
+      } else if (event.target.scrollLeft >= 630) {
+        arrowRight.classList.remove("arrow-active");
+      } else if (event.target.scrollLeft <= 630){
+        arrowRight.classList.add("arrow-active");
+      };
+
+
+});
+
+
