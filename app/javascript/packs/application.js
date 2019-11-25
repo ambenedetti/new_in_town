1dd4f82974894e992fb2e91c46e44674088e6690
Ana Maria Benedetti <ambenedetti@gmail.com>
2019-11-25 18:09:19 +0100

    fixed heroku problems and added hamburger navigation to navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,12 @@ import { search } from '../components/search';
 import { initAutocomplete } from '../components/autocomplete';
 import { initAutocompleteQuestions } from '../components/autocompleteQuestions';
 import { initAutocompleteTips } from '../components/autocompleteTips';
+import { hamburgerMenu } from '../components/navbar';
 
 const results = document.querySelector("#results");
 const selected = document.querySelector("#selected");
 const button = document.querySelector(".search-btn");
 
-initAutocomplete()
-initAutocompleteQuestions()
-initAutocompleteTips()
+initAutocomplete();
+initAutocompleteQuestions();
+initAutocompleteTips();
