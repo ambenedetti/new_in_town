59026e4ff97eb43821e12ab299270641649bc93e
gabi van den brule <gvandenbrule@gmail.com>
2019-11-27 17:04:06 +0100

    Autocomplete and Algolia are working everywhere

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -2,7 +2,6 @@ import places from 'places.js';
 
 const initAutocomplete = () => {
   const addressInput = document.querySelector('.places-search');
-
   if (addressInput) {
     places({ container: addressInput });
   }
